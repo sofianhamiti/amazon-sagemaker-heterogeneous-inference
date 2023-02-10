8dbdf7af56ad44f1c2a0aac671e28721721ecf2f
Sofian Hamiti <sofian.hamiti@gmail.com>
2023-02-10 17:04:52 +0000

    Delete triton.py

--- src/triton.py
+++
@@ -1,7 +0,0 @@
-from sagemaker_training import environment
-
-print("======== YO! ========")
-
-env = environment.Environment()
-print(f"current_instance_type={env.current_instance_type}")
-print(f"current_group_name={env.current_instance_group}")
